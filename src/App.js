3b64475997d57c2ea2b1ca4a35b3f65cee65facc
chukaka <barbarisdenis028@gmail.com>
2024-04-19 03:06:53 +0300

    first

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <div className="App">
-      <Stars />
+      {/* <Stars /> */}
       <Router>
         <Switch>
           <Route component={HomePage} path="/" exact />
