d1617e58577996397defed96832b3682afb84384
chukaka <barbarisdenis028@gmail.com>
2024-05-27 19:46:24 +0300

    adagd

--- src/components/Tasks/Egg/Egg.js
+++ src/components/Tasks/Egg/Egg.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
 import "./Egg.css";
-import kickLogo from "./IMG_0391.png";
+import kickLogo from "./IN.png";
 import UserContext from "../../../context/UserContext";
 import icons8 from '../../../pages/icons8-dollar-coin-94.png'
 import galachka from '../icons8-approval-94.png'
@@ -9,25 +9,25 @@ const Egg = () => {
   const { contextData } = useContext(UserContext);
   const { balance, setBalance } = contextData;
 
-  const [clicked, setClicked] = useState(localStorage.getItem('clickedEgg') === 'true');
+  const [clicked, setClicked] = useState(localStorage.getItem('clickedInu') === 'true');
 
   const handleClick = () => {
     if (!clicked) {
       setBalance(balance + 10.00);
-      localStorage.setItem('clickedEgg', 'true');
+      localStorage.setItem('clickedInu', 'true');
       setTimeout(() => {
         setClicked(true);
         
-      }, 2000); // 10 секунд задержки
+      }, 500); // 10 секунд задержки
     }
   };
 
   return (
     <div className={clicked ? "disabled" : ""} onClick={handleClick}>      
-        <a href="https://t.me/uniegg" className="egg-skin-container">
+        <a href="https://t.me/inusider" className="egg-skin-container">
           <img src={kickLogo} alt="Egg Skin" className="egg-skin-image" />
           <div className="egg-skin-info">
-            <p className="egg-skin-name">Зайти в игру и собрать 100 🥚</p>
+            <p className="egg-skin-name">Подписаться на INUSIDER</p>
             <div className="egg-skin-price">
               {!clicked && (
                 <>
