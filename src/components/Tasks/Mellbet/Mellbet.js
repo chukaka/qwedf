17e5285c88716dbaff37c86df7ed3fd41801f393
chukaka <barbarisdenis028@gmail.com>
2024-11-13 11:20:56 +0200

    afafA

--- src/components/Tasks/Mellbet/Mellbet.js
+++ src/components/Tasks/Mellbet/Mellbet.js
@@ -12,7 +12,7 @@ const Mellbet = () => {
       <img src={mellBet} alt="Mellbet Skin" className="mellbet-skin-image" />
 
       <div className="mellbet-skin-info">
-        <p className="mellbet-skin-name">Сделай дипозит в TON </p>
+        <p className="mellbet-skin-name">Сделай дипозит в TON и получи</p>
         <p className="mellbet-skin-price">
           <>
             <img src="https://cdn3d.iconscout.com/3d/premium/thumb/toncoin-ton-coin-3d-icon-download-in-png-blend-fbx-gltf-file-formats--btc-bitcoin-cryptocurrency-pack-science-technology-icons-7138701.png?f=webp" alt="Icon" className="mellbet-small-image" />
